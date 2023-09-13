aa4e495f522e2a21a8bc3b353d534a1be434df0f
knahar12bd@gmail.com <knahar12bd@gmail.com>
2023-09-13 14:57:43 +0600

    plugin added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'composer-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0e776e9ed724d9890300f0c5a59d875b528e4cec',
+        'name' => 'ruma/crud-kit',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ruma/crud-kit' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0e776e9ed724d9890300f0c5a59d875b528e4cec',
+            'dev_requirement' => false,
+        ),
+    ),
+);
